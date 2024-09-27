b3d3096e0dfe0b661e7a22ab9679cc6618b17c98
Michele Vomera <michele.vomera@outlook.com>
2024-09-27 10:31:12 +0200

    fix: flux validate update

--- scripts/flux-validate.sh
+++ scripts/flux-validate.sh
@@ -58,8 +58,8 @@ echo "INFO - Validating kustomize overlays"
 find . -type f -name $kustomize_config -print0 | while IFS= read -r -d $'\0' file;
   do
     echo "INFO - Validating kustomization ${file/%$kustomize_config}"
-    echo kubectl kustomize build "${file/%$kustomize_config}" "${kustomize_flags[@]}"
-    kubectl kustomize build "${file/%$kustomize_config}" "${kustomize_flags[@]}" | \
+    echo kustomize build "${file/%$kustomize_config}" "${kustomize_flags[@]}"
+    kustomize build "${file/%$kustomize_config}" "${kustomize_flags[@]}" | \
       kubeconform "${kubeconform_flags[@]}" "${kubeconform_config[@]}"
     if [[ ${PIPESTATUS[0]} != 0 ]]; then
       exit 1
